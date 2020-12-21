6616995f3454520a852f49aea4f4093b4904fb6f
Phellipe <phellipe.sander1@gmail.com>
2020-12-21 20:51:45 -0300

    base project and header created

---
+++ src/core/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import './styles.scss';
+
+const Header  = () => {
+  return (
+    <div className="header-container">
+      <h1 className="header-title">Bootcamp DevSuperior</h1>
+    </div>
+  );
+}
+
+export default Header;
